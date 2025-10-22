881d1940eec091df4876cabcd393a6447ebb474a
hiromichi333 <iwa.hiro3000@gmail.com>
2025-10-22 22:30:38 +0900

    refactor(controller): Admin::InquiriesController#indexメソッドの複雑度を改善
    - フィルタリング条件をprivateメソッドに抽出
    - valid_status?, valid_category?メソッドを追加
    - AbcSize: 25.63 → 制限内に収める
    - 機能は変更なし、全テスト通過（18 examples, 0 failures）

--- app/controllers/admin/inquiries_controller.rb
+++ app/controllers/admin/inquiries_controller.rb
@@ -6,17 +6,8 @@ module Admin
 
     def index
       @inquiries = Inquiry.includes(:user).recent
-
-      # 有効なステータス値のみ受け付ける
-      if params[:status].present? && Inquiry.statuses.key?(params[:status])
-        @inquiries = @inquiries.by_status(params[:status])
-      end
-
-      # 有効なカテゴリ値のみ受け付ける
-      if params[:category].present? && Inquiry.categories.key?(params[:category])
-        @inquiries = @inquiries.by_category(params[:category])
-      end
-
+      @inquiries = @inquiries.by_status(params[:status]) if valid_status?
+      @inquiries = @inquiries.by_category(params[:category]) if valid_category?
       @inquiries = @inquiries.page(params[:page])
     end
 
@@ -53,6 +44,14 @@ module Admin
       return if current_user&.admin?
 
       redirect_to user_root_path, alert: '管理者権限が必要です。'
+    end
+
+    def valid_status?
+      params[:status].present? && Inquiry.statuses.key?(params[:status])
+    end
+
+    def valid_category?
+      params[:category].present? && Inquiry.categories.key?(params[:category])
     end
   end
 end
